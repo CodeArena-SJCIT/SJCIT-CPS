ba745c05cf8b8d95970dffdaea46cafe4b2796bc
ADITYA SAROHA <aditya@localhost.localdomain>
2025-01-29 09:18:20 +0530

    Second Work, initiated the Proejct, Added the Login Page - Still work under Progress, added some more features in the login page, Like Tabs, and Progress bars

---
+++ src/CPS-Frontend/Login Page/Sign In Component/singnInComponent.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button, TextField, Select, MenuItem, FormControl, InputLabel, CircularProgress, LinearProgress, Tab, Tabs, Box } from "@mui/material";
+import { useState, useEffect } from "react";
+
+
+const SignInComponent = () => {
+
+    const [SignInButtonShow, setSignInButtonShow] = useState(true);
+    const [ShowCircularProgress, setShowCircularProgress] = useState(false);
+
+    return (
+        <>
+
+            <TextField label={<span>Institute Email <span style={{ color: "red" }}>*</span></span>} type='email' />
+            <TextField label={<span>Password <span style={{ color: "red" }}>*</span></span>} type='password' />
+
+            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                {SignInButtonShow && <Button size='large' variant='contained' color='info' fullWidth="false" onClick={() => { setSignInButtonShow(false); setShowCircularProgress(true); }} sx={{ textTransform: "none" }}>Sign In</Button>}
+                {ShowCircularProgress && <CircularProgress />}
+            </div>
+        </>
+    );
+}
+
+export default SignInComponent;
--- src/CPS-Frontend/Login Page/login.js
+++ src/CPS-Frontend/Login Page/login.js
@@ -1,49 +1,38 @@
 import React, { useState } from 'react';
-import { Button, TextField, Select, MenuItem, FormControl, InputLabel, CircularProgress } from "@mui/material";
+import { Button, TextField, Select, MenuItem, FormControl, InputLabel, CircularProgress, LinearProgress, Tab, Tabs, Box } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import "./sjcit-cps-login.css";
+import SignUpComponent from './Sign Up Component/signupComponent';
+import SignInComponent from './Sign In Component/singnInComponent';
 
 export const Login = () => {
 
-    const [selectedOptionForRole, setselectedOptionForRole] = useState('');
-    const [selectedOptionForDepartment, setselectedOptionForDepartment] = useState('');
-    const [a, seta] = useState(true);
-    const [b, setb] = useState(false);
+    const [tabValue, setTabValue] = useState(0);
 
-    const roles = ["Assistant Professor", "Associate Professor", "Professor"];
-    const departments = ["CSE", "ISE", "AI&ML", "AI&DS", "ECE", "CV", "AS", "AE"];
+    const handleTabValueChange = (event, newValue) => {
+        setTabValue(newValue);
+    }
+
 
     return (
+
         <div className='main-login-div'>
 
+            <Tabs value={tabValue} onChange={handleTabValueChange} textColor='primary' indicatorColor='primary' centered sx={{ textTransform: "none" }}>
+                <Tab label="Sign In" sx={{ textTransform: "none" }}></Tab>
+                <Tab label="Sign Up" sx={{ textTransform: "none" }}></Tab>
+            </Tabs>
 
-            <TextField label="Name" required />
-            <TextField label="Email" type='email' required />
-            <TextField label="Set Password" type='password' required />
-            <TextField label="Confirm Password" type='password' required />
+            {tabValue == 0 && (
+                <SignInComponent />
+            )}
 
-            <FormControl fullWidth required>
-                <InputLabel id="input-label-for-selecting-role">Select Role</InputLabel>
-                <Select labelId='input-label-for-selecting-role' id='simple-select-role' label="Select Role" value={selectedOptionForRole} onChange={(event) => { setselectedOptionForRole(event.target.value) }}> Select your Role
-                    {roles.map((value) => (
-                        <MenuItem key={value} value={value}>{value}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+            {tabValue == 1 && (
 
-            <FormControl fullWidth required>
-                <InputLabel id="input-label-for-selecting-department">Select Department</InputLabel>
-                <Select labelId='input-label-for-selecting-department' id='simple-select-department' label="Select Department" value={selectedOptionForDepartment} onChange={(event) => { setselectedOptionForDepartment(event.target.value) }}>
-                    {departments.map((value) => (
-                        <MenuItem key={value} value={value}>{value}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+                <SignUpComponent />
 
-            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-                {a && <Button size='large' variant='contained' color='info' fullWidth="false" onClick={() => { seta(false); setb(true); }}>Sign Up</Button>}
-                {b && <CircularProgress />}
-            </div>
+            )}
+
 
         </div >
     )
