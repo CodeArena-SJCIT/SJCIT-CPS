a6b88832988b0d356965aefbfb924bc669a2e9bd
Vishnu-dutt <ankitkumarrai769631@gmail.com>
2025-02-04 22:03:57 +0530

    login page animation fade in fade out and file format impovement using Prettier

--- src/CPS-Frontend/Main Form/Adminstration - 1/adminstration1.js
+++ src/CPS-Frontend/Main Form/Adminstration - 1/adminstration1.js
@@ -1,27 +1,61 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import { Box, Tooltip, Button, FormGroup, TextField, FormControl, Select, InputLabel, MenuItem, Checkbox, Divider, Card, CardContent, CardMedia, Typography, CardHeader, CardActions, FormControlLabel } from "@mui/material";
+import {
+  Box,
+  Tooltip,
+  Button,
+  FormGroup,
+  TextField,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+  Checkbox,
+  Divider,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardHeader,
+  CardActions,
+  FormControlLabel,
+} from "@mui/material";
 
 const Administration1 = () => {
+  const [adminstrationOneChecked, setadminstrationOneChecked] = useState(false);
 
-    const [adminstrationOneChecked, setadminstrationOneChecked] = useState(false);
+  const handleadminstrationOneChecked = (e) => {
+    setadminstrationOneChecked(e.target.checked);
+  };
 
-    const handleadminstrationOneChecked = (e) => {
-        setadminstrationOneChecked(e.target.checked);
-    }
+  return (
+    <>
+      <Card
+        variant="elevation"
+        elevation={5}
+        sx={{ width: "100%", paddingLeft: "40px", paddingRight: "40px" }}
+        raised
+      >
+        <CardHeader
+          title="HOD, Dean, COE, Chief Warden, Director, Associate Director (Research), IQAC Coordinator."
+          subheader="Select Options"
+          sx={{ textAlign: "center" }}
+        />
 
-    return (
-        <>
-            <Card variant="elevation" elevation={5} sx={{ width: '100%', paddingLeft: "40px", paddingRight: "40px" }} raised>
-                <CardHeader title="HOD, Dean, COE, Chief Warden, Director, Associate Director (Research), IQAC Coordinator." subheader="Select Options" sx={{ textAlign: "center" }} />
-
-                <FormGroup sx={{ marginTop: "10px", marginBottom: "20px" }}>
-                    <FormControlLabel control={<Checkbox checked={adminstrationOneChecked} onChange={(e) => handleadminstrationOneChecked(e)} />} label="Do you have any positions of HOD, Dean, COE, Chief Warden, Director, Associate Director (Research), IQAC Coordinator?" />
-                </FormGroup>
-            </Card>
-
-        </>
-    );
-}
+        <FormGroup sx={{ marginTop: "10px", marginBottom: "20px" }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={adminstrationOneChecked}
+                onChange={(e) => handleadminstrationOneChecked(e)}
+              />
+            }
+            label="Do you have any positions of HOD, Dean, COE, Chief Warden, Director, Associate Director (Research), IQAC Coordinator?"
+          />
+        </FormGroup>
+      </Card>
+    </>
+  );
+};
 
 export default Administration1;
