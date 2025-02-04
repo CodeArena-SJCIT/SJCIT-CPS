209e971421071f9e54870f9af2eec57dfd8e44ae
Aditya Saroha <143502508+Aditya-138-12@users.noreply.github.com>
2025-02-04 22:50:06 +0530

    Revert "login page impovements"

--- src/CPS-Frontend/Login Page/login.js
+++ src/CPS-Frontend/Login Page/login.js
@@ -1,53 +1,39 @@
-import React, { useState } from "react";
-import {
-  Button,
-  TextField,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
-  CircularProgress,
-  LinearProgress,
-  Tab,
-  Tabs,
-  Box,
-} from "@mui/material";
+import React, { useState } from 'react';
+import { Button, TextField, Select, MenuItem, FormControl, InputLabel, CircularProgress, LinearProgress, Tab, Tabs, Box } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import "./sjcit-cps-login.css";
-import SignUpComponent from "./Sign Up Component/signupComponent";
-import SignInComponent from "./Sign In Component/singnInComponent";
+import SignUpComponent from './Sign Up Component/signupComponent';
+import SignInComponent from './Sign In Component/singnInComponent';
 
 export const Login = () => {
-  const [tabValue, setTabValue] = useState(0);
-  const [fade, setFade] = useState(true);
 
-  const handleTabValueChange = (event, newValue) => {
-    setFade(false);
+    const [tabValue, setTabValue] = useState(0);
 
-    setTimeout(() => {
-      setTabValue(newValue);
-      setFade(true);
-    }, 300);
-  };
+    const handleTabValueChange = (event, newValue) => {
+        setTabValue(newValue);
+    }
 
-  return (
-    <div className="main-login-div">
-      <Tabs
-        value={tabValue}
-        onChange={handleTabValueChange}
-        textColor="primary"
-        indicatorColor="primary"
-        centered
-        sx={{ textTransform: "none" }}
-      >
-        <Tab label="Sign In" sx={{ textTransform: "none" }}></Tab>
-        <Tab label="Sign Up" sx={{ textTransform: "none" }}></Tab>
-      </Tabs>
 
-      {/* Apply Animation class */}
-      <div className={`form-container ${fade ? "fade-in" : "fade-out"}`}>
-        {tabValue === 0 ? <SignInComponent /> : <SignUpComponent />}
-      </div>
-    </div>
-  );
-};
+    return (
+
+        <div className='main-login-div'>
+
+            <Tabs value={tabValue} onChange={handleTabValueChange} textColor='primary' indicatorColor='primary' centered sx={{ textTransform: "none" }}>
+                <Tab label="Sign In" sx={{ textTransform: "none" }}></Tab>
+                <Tab label="Sign Up" sx={{ textTransform: "none" }}></Tab>
+            </Tabs>
+
+            {tabValue == 0 && (
+                <SignInComponent />
+            )}
+
+            {tabValue == 1 && (
+
+                <SignUpComponent />
+
+            )}
+
+
+        </div >
+    )
+}
