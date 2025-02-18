6238e1db37c6778fd5873930b81b589b7dd5b8a2
Vishnu-dutt <ankitkumarrai769631@gmail.com>
2025-02-18 17:20:53 +0530

    trying to make landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Login } from './CPS-Frontend/Login Page/login';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Form from './CPS-Frontend/Main Form/form';
+import Landing from './CPS-Frontend/landing/landing';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
           <Route path="/login" element={<Login />} />
           <Route path='/form/:typeid' element={<Form />} />
+          <Route path='/' element={<Landing/>} />
 
         </Routes>
       </Router>
